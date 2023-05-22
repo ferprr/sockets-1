3709d72e753efa58f112da31bb8f8c93682ea6e7
Fernanda Pereira <fernandacarolina.fcsp@gmail.com>
2023-05-22 00:13:49 -0300

    client sending file to server

--- client.c
+++ client.c
@@ -17,7 +17,6 @@ int is_valid_extension(const char *filename)
     int num_valid_extensions = sizeof(valid_extensions) / sizeof(valid_extensions[0]);
 
     const char *extension = strrchr(filename, '.'); // retorna ponteiro para '.' encontrado no filename
-    puts(extension);
     if (extension != NULL)
     {
         for (int i = 0; i < num_valid_extensions; i++)
@@ -73,6 +72,8 @@ int main(int argc, char **argv)
     while (1)
     {
         char buf[BUFSZ];
+        char selected_file[100];
+
         memset(buf, 0, BUFSZ);
         printf("command> ");
         fgets(buf, BUFSZ - 1, stdin);
@@ -93,7 +94,6 @@ int main(int argc, char **argv)
                 // Verifica se o arquivo é válido (extensões aceitáveis)
                 if (is_valid_extension(filename) == 0)
                 {
-                    puts(filename);
                     printf("%s not valid!\n", filename);
                     continue;
                 }
@@ -101,67 +101,53 @@ int main(int argc, char **argv)
                 {
                     printf("%s selected\n", filename);
                     selected = 1;
+                    strcpy(selected_file, filename); //na proxima iteração não vai estar com essa cópia, limpou
                 }
-
-                printf("passou nas ver\n");
             }
             else
             {
                 printf("no file selected.\n");
             }
-
-            // send(s, buf, strlen(buf), 0);
         }
         else if (strcmp(buf, "send file") == 0 && selected)
         {
-            printf("pronto apra enviar\n");
-            //     // Recebe o arquivo do cliente
-            //     memset(buf, 0, BUFSZ);
-            //     if (recv(clientSocket, buffer, MAX_BUFFER_SIZE, 0) < 0)
-            //     {
-            //         perror("Erro ao receber arquivo do cliente");
-            //         break;
-            //     }
+            // Envia o nome do arquivo para o servidor
+            if (send(s, selected_file, strlen(selected_file), 0) < 0)
+            {
+                perror("Erro ao enviar nome do arquivo para o servidor");
+                break;
+            }
 
-            //     // Extrai o nome do arquivo
-            //     char *filename = buffer + 5;
+            // Abre o arquivo para leitura
+            FILE *file = fopen(selected_file, "rb");
+            if (file == NULL)
+            {
+                //puts(selected_file);
+                printf("%s does not exist\n", selected_file);
+                continue;
+            }
 
-            //     // Abre o arquivo para escrita
-            //     file = fopen(filename, "wb");
-            //     if (file == NULL)
-            //     {
-            //         sprintf(buffer, "error receiving file %s", filename);
-            //         send(clientSocket, buffer, strlen(buffer), 0);
-            //         break;
-            //     }
+            // Lê e envia o conteúdo do arquivo para o servidor
+            while (1)
+            {
+                // Lê dados do arquivo
+                int bytesRead = fread(buf, 1, BUFSZ, file);
 
-            //     // Recebe e armazena o conteúdo do arquivo
-            //     while (1)
-            //     {
-            //         memset(buffer, 0, MAX_BUFFER_SIZE);
-            //         int bytesRead = recv(clientSocket, buffer, MAX_BUFFER_SIZE, 0);
-            //         if (bytesRead < 0)
-            //         {
-            //             perror("Erro ao receber arquivo do cliente");
-            //             break;
-            //         }
-            //         if (bytesRead == 0)
-            //         {
-            //             break; // Fim do arquivo
-            //         }
-            //         fwrite(buffer, 1, bytesRead, file);
-            //     }
+                // Envia dados para o servidor
+                if (send(s, buf, bytesRead, 0) < 0)
+                {
+                    perror("Erro ao enviar arquivo para o servidor");
+                    break;
+                }
 
-            //     fclose(file);
+                // Verifica se chegou ao final do arquivo
+                if (bytesRead < BUFSZ)
+                {
+                    break;
+                }
+            }
 
-            //     // Envia a resposta ao cliente
-            //     sprintf(buffer, "file %s received", filename);
-            //     send(clientSocket, buffer, strlen(buffer), 0);
-            // }
-            // else
-            // {
-            //     sprintf(buffer, "Comando desconhecido");
-            //     send(clientSocket, buffer, strlen(buffer), 0);
+            fclose(file);
         }
         else if (strcmp(buf, "send file") == 0 && !selected)
         {
